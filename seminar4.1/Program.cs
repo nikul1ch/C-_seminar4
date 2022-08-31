2bfb351f3b89f3b99a7726d036a904fa555f7f4d
nikul1ch <nikulin@netkam.ru>
2022-08-31 10:55:08 +0300

    Цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.

--- seminar4.1/Program.cs
+++ seminar4.1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.Write("Введите число: ");
+﻿Console.Write("Введите число А: ");
 int A = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите степень: ");
+Console.Write("Введите число B: ");
 int B = Convert.ToInt32(Console.ReadLine());
 int C = A;
 for (int i = 1; i < B; i++) C = C * A;
