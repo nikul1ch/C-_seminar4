68d31cfced2907efd9f3797aaa6c077d9242ec2d
nikul1ch <nikulin@netkam.ru>
2022-08-31 13:35:31 +0300

    Программа, которая принимает на вход число и выдаёт сумму цифр в числе

--- seminar4.2/Program.cs
+++ seminar4.2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int temp = 0;  //Хотел сделать инициализацию внутри метода, но он постоянно обнуляется при рекурсии, как это можно обойти?
+
+int SumDigits(int digit)
+{
+    if (digit < 10) return temp += digit;
+    else
+    {
+        temp += digit % 10;
+        SumDigits(digit / 10);
+    }
+    return temp;
+}
+
+void PrintConsole(int digit)
+{
+    Console.Write($"Сумма цифр введённого числа = {digit}");
+}
+
+PrintConsole(SumDigits(A));
