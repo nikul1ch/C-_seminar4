477580323938fe24a6a59ce8986cf8f9c9e7fe72
nikul1ch <nikulin@netkam.ru>
2022-08-31 14:00:47 +0300

    Программа, которая задаёт массив из 8 элементов и выводит их на экран.

--- seminar4.3/Program.cs
+++ seminar4.3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] Arr = new int[8];
+Random rand = new Random();
+
+void FillingArrayRandom(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) array[i] = rand.Next(-100, 100);
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1) Console.Write(array[i]);
+        else Console.Write(array[i] + ", ");
+    }
+    Console.Write(" ]");
+}
+
+FillingArrayRandom(Arr);
+PrintArray(Arr);
