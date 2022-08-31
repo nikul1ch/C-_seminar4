c73fc48ad8e5fa2f85dd19d66ee883ab89d24855
nikul1ch <nikulin@netkam.ru>
2022-08-31 10:52:21 +0300

    first commit

---
+++ seminar4.1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int C = A;
+for (int i = 1; i < B; i++) C = C * A;
+Console.Write($"Число {A} в степени {B} = {C}");
