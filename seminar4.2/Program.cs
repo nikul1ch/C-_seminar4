5a03db653edaeaaeebf1ff057968c1efee8e5cf5
nikul1ch <nikulin@netkam.ru>
2022-09-01 15:40:52 +0300

    решение оптимизировано

--- seminar4.2/Program.cs
+++ seminar4.2/Program.cs
@@ -1,17 +1,12 @@
 ﻿Console.Write("Введите число: ");
 int A = Convert.ToInt32(Console.ReadLine());
-int temp = 0;  //Хотел сделать инициализацию внутри метода, но он постоянно обнуляется при рекурсии, как это можно обойти?
 
 int SumDigits(int digit)
 {
-    if (digit < 10) return temp += digit;
-    else
-    {
-        temp += digit % 10;
-        SumDigits(digit / 10);
-    }
-    return temp;
+    if (digit == 0) return digit;
+    return (digit % 10 + SumDigits(digit / 10));
 }
+
 
 void PrintConsole(int digit)
 {
